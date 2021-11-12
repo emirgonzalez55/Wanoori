7db19dd88210ff252a438920810de9a145cc57b6
emirgonzalez55 <33497133+emirgonzalez55@users.noreply.github.com>
2021-11-11 21:27:58 -0300

    Contenido nuevo

---
+++ contenido/juegos/lol.php
@@ -0,0 +1,32 @@
+<?php
+
+include '../base/base.php'
+ ?>  
+<!doctype html>
+<head>
+  <title>League of Legends</title>
+</head> 
+<body>
+
+
+
+<div class="mb-3 pelicula mensaje" style="max-width: 1052px;">
+  <div class="row g-0">
+    <div class="col-md-3">
+      <img src="img/lol.jpg"  class="img-fluid rounded-start img-fluid" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+      <h1>League of Legends(2009)</h1>
+      <p>Año:  2009</p>
+      <p>MOBA, Videojuego de rol de acción </p>
+      <h4>sinopsis:</h4>
+      <p class="line-clamp">League of Legends (también conocido por sus siglas LoL) es un videojuego del género multijugador de arena de batalla en línea (MOBA) y deporte electrónico el cual fue desarrollado por Riot Games para Microsoft Windows y OS X.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="trailer" style="max-width: 100%;"  >
+<iframe width="100%" height="100%" src="https://www.youtube.com/embed/aR-KAldshAE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+</html>
---
+++ contenido/peliculas/regresoalfuturo.php
@@ -0,0 +1,33 @@
+<?php
+
+include '../base/base.php'
+ ?>  
+<!doctype html>
+<head>
+  <title>regreso al futuro</title>
+</head> 
+<body>
+
+
+
+<div class="mb-3 pelicula mensaje" style="max-width: 1052px;">
+  <div class="row g-0">
+    <div class="col-md-3">
+      <img src="img/Regresoalfuturo.jpg"  class="img-fluid rounded-start img-fluid" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+      <h1>Regreso al futuro (1985)</h1>
+      <p>Año: 1985</p>
+      <p>Géneros: Aventura Ciencia Ficción Comedia </p>
+      <p>Reparto: Michael J. Fox...</p>
+      <h4>sinopsis:</h4>
+      <p class="line-clamp">Marty McFly es un adolescente amigo de Doc, un científico a los que todos menos él toman por chiflado. Cuando Doc crea una máquina para viajar en el tiempo en forma de un automóvil deportivo, Marty viaja accidentalmente al año 1955. Allí se encontrará con sus padres cuando eran jóvenes, pero impide su primer encuentro. A partir de entonces, y con el tiempo en su contra, McFly deberá conseguir que se enamoren para que se casen y él no deje de existir.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="trailer" style="max-width: 100%;"  >
+<iframe width="100%" height="100%" src="https://www.youtube.com/embed/q27OhNUzBXk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+</html>
