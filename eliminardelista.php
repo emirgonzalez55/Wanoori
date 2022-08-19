ca54208d9bac3c2e8d5ffd218176633c65a0a401
emirgonzalez55 <emigonzalez55@gmail.com>
2022-08-19 19:19:29 -0300

    Corrección de errores

--- eliminardelista.php
+++ eliminardelista.php
@@ -1,21 +1,42 @@
-<?php  
+<?php 
+	session_start();
+
+	if (isset($_SESSION['idusuario'])) {
+
+	}else
+		header("Location:index.php");
+
 	if (!isset($_GET['idfavorito'])) {
 		exit();
 	}
-
+	$idusuario = $_SESSION['idusuario'];
 	$idfavorito = $_GET['idfavorito'];
-	$contenido = $_GET['contenido'];
-	include 'bd.php';
-	$em = $conexion->prepare("DELETE FROM favoritos WHERE idfavorito = ?;");
-	$result = $em->execute([$idfavorito]);
-
-	if ($result === TRUE && !empty($contenido)) {
-		header("Location: contenido.php?contenido=$contenido");
-	}else{
-		echo "Error";
+	if(!empty($_GET['contenido'])){
+		$contenido = $_GET['contenido'];
 	}
-	if(empty($contenido)){
+
+	include 'bd.php';
+	$em = $conexion->prepare("DELETE FROM favoritos WHERE idfavorito = ? AND idusuario = ?;");
+	$result = $em->execute([$idfavorito, $idusuario ]);
+	$delete = $em->rowCount();
+
+	if ($delete === 1 && !empty($contenido)) {
+		header("Location: contenido.php?contenido=$contenido");
+	}
+	elseif ($delete === 1 && empty($contenido)) {
 		header("Location: milista.php");
+
+	}elseif (empty($contenido) && $delete === 0) {
+		print("ERROR 404");
+
+	}else{
+		print("ERROR 404 ");
+	}
+
+	if ($result === TRUE) {
+
+	}else{
+		print ("Error");
 	}
 
 ?>
